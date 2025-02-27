62edf9d9a01a54d2ed655823a7d2b03887d4b2a9
Mohith310 <mohith1709@gmail.com>
2025-02-27 14:31:50 +0530

    Complete sign up functionality

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],server: {
+    proxy: {
+      '/backEnd': {
+        target: 'http://localhost:3000',
+        secure: false
+      }
+    }
+  }
 })
