892ef674787d4876e3aa0da6bc441e330ffaa445
Mohith310 <mohith1709@gmail.com>
2025-05-06 18:27:43 +0530

    Complete sign up page functionality

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -8,4 +8,13 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
+  server: {
+    proxy: {
+      '/backEnd': {
+        target: 'http://localhost:3000',
+        // changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
